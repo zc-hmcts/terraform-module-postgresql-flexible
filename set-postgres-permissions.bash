0f29486139e58c920f511450bbb7bb0668811caf
Zoe Cope <zoe.cope@hmcts.net>
2023-02-20 12:41:51 +0000

    cat

--- set-postgres-permissions.bash
+++ set-postgres-permissions.bash
@@ -11,7 +11,10 @@ pwd
 env
 
 export AZURE_CONFIG_DIR=~/.azure-db-manager
-az login --identity --verbose --tenant 531ff96d-0ae9-462a-8d2d-bec7c0b42082
+
+cat ~/.azure-db-manager
+
+az login --identity --verbose
 
 # shellcheck disable=SC2155
 export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query accessToken -o tsv)
